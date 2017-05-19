a14f0406a0f79bc7cc6700ab81edc7d52879c910
Peter Svensson <peter.svensson@stenaline.io>
2017-05-19 11:02:08 +0200

    The new version of FridgeAPI

---
+++ FridgeAPI/Controllers/FridgeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fridge;
+
+namespace FridgeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class FridgeController : Controller
+    {
+        private FridgeWorker _fridgeWorker;
+
+        public FridgeController()
+        {
+            _fridgeWorker = new FridgeWorker();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public FridgeInventory Get(string name)
+        {
+            return _fridgeWorker.GetInventoryItem(name);
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}/{quantity}")]
+        public bool Get(string name, double quantity)
+        {
+            return _fridgeWorker.IsItemAvailable(name, quantity);
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]FridgeInventory item)
+        {
+            _fridgeWorker.AddIngredientToFridge(item);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{name}")]
+        public void Put(string name, [FromBody]FridgeInventory item)
+        {
+            _fridgeWorker.AddIngredientToFridge(item);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{name}")]
+        public void Delete(string name, [FromBody] FridgeInventory item)
+        {
+            _fridgeWorker.TakeItemFromFridge(item.Name, item.Quantity);
+        }
+    }
+}
